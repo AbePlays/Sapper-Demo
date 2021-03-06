e05390a354ec811a8b8f3f0a91f40527622df879
Abe <abhi.rawat456@gmail.com>
2021-03-06 17:05:15 +0530

    Added home component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,76 @@
 </script>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>GOT</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="home-wrapper">
+  <div class="home-content">
+    <h1>Game of thrones</h1>
+    <p>
+      Game of Thrones is an American fantasy drama television series created by
+      David Benioff and D. B. Weiss for HBO. Game of Thrones is roughly based on
+      the storylines of the A Song of Ice and Fire book series by George R. R.
+      Martin, set in the fictional Seven Kingdoms of Westeros and the continent
+      of Essos.
+    </p>
+    <div class="divider" />
+    <div class="bottom-text">
+      <h3>Game of thrones</h3>
+      <p>2011-2019</p>
+    </div>
+  </div>
+</div>
 
 <style>
+  .home-wrapper {
+    background-color: black;
+    height: 90vh;
+    color: white;
+  }
+
+  .home-content {
+    max-width: 64rem;
+    margin: 0 auto;
+    padding: 1rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    position: relative;
+  }
+
+  .home-content h1 {
+    text-transform: uppercase;
+    letter-spacing: 0.3rem;
+    font-size: 5rem;
+    font-weight: 400;
+  }
+
+  .home-content p {
+    max-width: 50rem;
+    margin: 5rem 0;
+    line-height: 1.5;
+  }
+
+  .divider {
+    width: 100%;
+    height: 0.5rem;
+    background-color: white;
+  }
+
+  .bottom-text {
+    margin-top: 5rem;
+    color: goldenrod;
+    font-size: 0.8rem;
+  }
+
+  .bottom-text h3 {
+    text-transform: uppercase;
+  }
+
+  .bottom-text p {
+    margin-top: 0.5rem;
+  }
 </style>
