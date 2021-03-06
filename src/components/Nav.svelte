51b5bd45db8862398da51f3cc1879f84ff4b0e95
Abe <abhi.rawat456@gmail.com>
2021-03-06 16:39:27 +0530

    Project Setup

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,71 @@
 <script>
-	export let segment;
+  export let segment;
 </script>
 
-<style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
-</style>
-
 <nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
+  <div class="wrapper">
+    <div class="logo">
+      <h1>GOT</h1>
+    </div>
+    <ul>
+      <li>
+        <a aria-current={segment === "about" ? "page" : undefined} href="about"
+          >Characters</a
+        >
+      </li>
+      <li>
+        <a aria-current={segment === "about" ? "page" : undefined} href="about"
+          >Houses</a
+        >
+      </li>
+      <li>
+        <a aria-current={segment === "about" ? "page" : undefined} href="about"
+          >Books</a
+        >
+      </li>
+      <li>
+        <a aria-current={segment === "about" ? "page" : undefined} href="about"
+          >About</a
+        >
+      </li>
+    </ul>
+  </div>
 </nav>
+
+<style>
+  nav {
+    height: 10vh;
+    background-color: black;
+  }
+
+  .wrapper {
+    max-width: 80rem;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  h1 {
+    color: white;
+  }
+
+  ul {
+    display: flex;
+    align-items: center;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  li:not(:last-child) {
+    margin-right: 1rem;
+  }
+
+  li a {
+    text-decoration: none;
+    color: white;
+  }
+</style>
